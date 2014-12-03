38edb2d3f1c47df08bc74a524f0324d19eed5a89
jklughammer <jklughammer@cemm.oeaw.ac.at>
2014-12-03 20:35:29 +0100

    Update parseVCF.sh

--- parseVCF.sh
+++ parseVCF.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 in_vcf=$1
-out_vcf=${in_vcf/.vcf/_parsed2.vcf}
+out_vcf=${in_vcf/.vcf/_parsed.vcf}
 
 awk 'match($8, /(SNPEFF_EFFECT=[^;]*).+(SNPEFF_FUNCTIONAL_CLASS=[^;]*).+(SNPEFF_GENE_NAME=[^;]*).+(SNPEFF_IMPACT=[^;]*)/,arr) {split(arr[1],e1,"=") split(arr[2],e2,"=") split(arr[3],e3,"=") split(arr[4],e4,"=");print $1"\t"$2"\t"$3"\t"$4"\t"$5"\t"$6"\t"$7"\t"e1[2]"\t"e2[2]"\t"e4[2]"\t"e3[2]}' $in_vcf>$out_vcf
 
